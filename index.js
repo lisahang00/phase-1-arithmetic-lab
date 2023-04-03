b9d05b35a5f6261f484e0649064c4cc01d1115e9
Lisa Hang <lisahang13@gmail.com>
2023-04-02 17:15:19 -0700

    completed arith lab

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+{let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2}
 
-// Write your code here
+{let random = 10
+Math.floor(Math.random()*10) + 1;}
+let max = (1, 2,10, 16,18,20)
+
+{let num3 = 12;
+let num4 = 3;
+let mod = num3 / num4}
